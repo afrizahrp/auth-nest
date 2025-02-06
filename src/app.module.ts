2e5b846454eb3861d86a19c659d33c474845c3cb
afrizahrp <afriza.hrp@gmail.com>
2025-02-06 14:48:14 +0700

    update End Of Line Sequence Settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 // import { UserModule } from './user/user.module';
 // import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
     // UserModule,
     // AuthModule,
   ],
